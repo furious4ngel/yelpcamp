677db1d568e44bb8ed1021203fa72b7192ef01d3
t4rantino <stephanie.anoruigwe@gmail.com>
2022-11-24 08:16:01 -0800

    Handle connections
    Mongoose listens for initial connections with mongoose.connect() and subsequent connections with mongoose.connection.on(). I want to handle connection errors, open connections, and disconnections on the default Mongoose connection.

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 
-const database = 'yelpcamp';
-mongoose.connect(`mongodb://localhost:27017/${database}`)
-  .then(() => console.log(`Connected to ${database} database.`))
+const DATABASE_NAME = 'yelpcamp';
+mongoose.connect(`mongodb://localhost:27017/${DATABASE_NAME}`)
   .catch((error) => console.log(error));
+
+const connection = mongoose.connection;
+connection.on('error', error => console.error(error));
+connection.on('open', () => console.log(`Connected to ${DATABASE_NAME} database.`));
+connection.on('disconnected', () => console.log(`Disconnected from ${DATABASE_NAME} database.`));
 
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
